13c0c418607b0e052c0db73a2c8cba683fbe700a
jaycemxy <jayce_ma.xa@foxmail.com>
2018-06-05 15:57:10 +0800

    use https

--- song.js
+++ song.js
@@ -19,7 +19,7 @@ $(function(){
     })
 
     let audio = document.createElement('audio')
-    audio.src="http://dl.stream.qqmusic.qq.com/C4000012wpTL1jLNhJ.m4a?vkey=EB85F877C3CA715A0A012954B88A68C5585DEED0D3102B3916E1B0904CEAC7EE7E6D344FE90EE296F0A82D0DF2F5E62944B5C5894333825C&guid=5035692450&uin=0&fromtag=66"
+    audio.src="//dl.stream.qqmusic.qq.com/C4000012wpTL1jLNhJ.m4a?vkey=EB85F877C3CA715A0A012954B88A68C5585DEED0D3102B3916E1B0904CEAC7EE7E6D344FE90EE296F0A82D0DF2F5E62944B5C5894333825C&guid=5035692450&uin=0&fromtag=66"
     audio.oncanplay = function(){
         audio.play()
         $('.disc-container').addClass('playing')
